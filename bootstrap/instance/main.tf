01d83dca830b74b886faecc5ea8a03faa992937a
Felipe Gonzalez <gonzalezz_felipe@hotmail.com>
2024-03-01 13:36:59 -0300

    chore: Add instance bootstrap (#2)
    * chore: Add instance bootstrap
    
    * Add service

---
+++ bootstrap/instance/main.tf
@@ -0,0 +1,86 @@
+variable "namespace" {
+  type = string
+}
+
+variable "network" {
+  type = string
+
+  validation {
+    condition     = contains(["mainnet", "preprod", "preview"], var.network)
+    error_message = "Invalid network. Allowed values are mainnet, preprod and preview."
+  }
+}
+
+variable "salt" {
+  type = string
+}
+
+variable "dbsync_secret_name" {
+  type = string
+}
+
+variable "image_tag" {
+  type = string
+}
+
+variable "replicas" {
+  type    = number
+  default = 1
+}
+
+variable "server_port" {
+  type    = number
+  default = 3000
+}
+
+variable "server_debug" {
+  type    = bool
+  default = false
+}
+
+variable "server_prometheus_metrics" {
+  type    = bool
+  default = true
+}
+
+variable "dbsync_host" {
+  type = string
+}
+
+variable "dbsync_database" {
+  type    = string
+  default = "cardanodbsync"
+}
+
+variable "dbsync_max_conn" {
+  type    = number
+  default = 10
+}
+
+variable "token_registry_url" {
+  type    = string
+  default = "https://tokens.cardano.org"
+}
+
+variable "resources" {
+  type = object({
+    limits = object({
+      cpu    = string
+      memory = string
+    })
+    requests = object({
+      cpu    = string
+      memory = string
+    })
+  })
+  default = {
+    limits : {
+      cpu : "200m"
+      memory : "400Mi"
+    }
+    requests : {
+      cpu : "200m"
+      memory : "400Mi"
+    }
+  }
+}
