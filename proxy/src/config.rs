c362c65b68051c6cfb19f970f9eb9c4c26a0ff1c
Felipe Gonzalez <gonzalezz_felipe@hotmail.com>
2024-03-25 17:19:38 -0300

    fix: Improving tiers background service (#26)

--- proxy/src/config.rs
+++ proxy/src/config.rs
@@ -1,4 +1,4 @@
-use std::{env, path::PathBuf};
+use std::{env, path::PathBuf, time::Duration};
 
 use crate::forbidden_endpoints::ForbiddenEndpoint;
 
@@ -7,6 +7,7 @@ pub struct Config {
     pub proxy_addr: String,
     pub proxy_namespace: String,
     pub proxy_tiers_path: PathBuf,
+    pub proxy_tiers_poll_interval: Duration,
     pub prometheus_addr: String,
     pub ssl_crt_path: String,
     pub ssl_key_path: String,
@@ -28,6 +29,14 @@ impl Config {
             proxy_tiers_path: env::var("PROXY_TIERS_PATH")
                 .map(|v| v.into())
                 .expect("PROXY_TIERS_PATH must be set"),
+            proxy_tiers_poll_interval: env::var("PROXY_TIERS_POLL_INTERVAL")
+                .map(|v| {
+                    Duration::from_secs(
+                        v.parse::<u64>()
+                            .expect("PROXY_TIERS_POLL_INTERVAL must be a number in seconds. eg: 2"),
+                    )
+                })
+                .unwrap_or(Duration::from_secs(2)),
             prometheus_addr: env::var("PROMETHEUS_ADDR").expect("PROMETHEUS_ADDR must be set"),
             ssl_crt_path: env::var("SSL_CRT_PATH").expect("SSL_CRT_PATH must be set"),
             ssl_key_path: env::var("SSL_KEY_PATH").expect("SSL_KEY_PATH must be set"),
